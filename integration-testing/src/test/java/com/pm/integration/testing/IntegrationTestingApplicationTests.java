317dd95585d577a55c8f99429ffe6836d2a75ff4
kishorenanda518 <kishorenanda518@gmail.com>
2025-06-06 13:33:48 +0530

    integration-testing

---
+++ integration-testing/src/test/java/com/pm/integration/testing/IntegrationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.integration.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
